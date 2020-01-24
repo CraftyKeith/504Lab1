7b13a0a3399f32b1d99c750db83471177db0d0c2
CraftyKeith <43795203+CraftyKeith@users.noreply.github.com>
2020-01-23 18:33:07 -0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,12 +1,29 @@
-var map = L.map('map').fitWorld();
+// var map = L.map('map').fitWorld();
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={pk.eyJ1IjoiY3JhZnR5a2VpdGgiLCJhIjoiY2s1aGp5bHV5MDRrdjNqbW41dDE0enEybyJ9.st8ZnB2PCZ17FSVTLcZQtg}', {
+
+var light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWpzbGFnZXIiLCJhIjoiZUMtVjV1ZyJ9.2uJjlUi0OttNighmI-8ZlQ', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v10',
+    id:'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
-}).addTo(map);
+});
+
+var dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWpzbGFnZXIiLCJhIjoiZUMtVjV1ZyJ9.2uJjlUi0OttNighmI-8ZlQ', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id:'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+});
+var map = L.map('map', {layers:[light, dark]});
+
+var baseMaps={
+  "light": light,
+  "dark": dark
+};
+
+L.control.layers(baseMaps).addTo(map);
 
 function onLocationFound(e) {
     var radius = e.accuracy; //this defines a variable radius as the accuracy value returned by the locate method divided by 2. It is divided by 2 because the accuracy value is the sum of the estimated accuracy of the latitude plus the estimated accuracy of the longitude. The unit is meters.
@@ -20,13 +37,26 @@ function onLocationFound(e) {
           else{
               L.circle(e.latlng, radius, {color: 'red'}).addTo(map);
           }
+          var times = SunCalc.getTimes(new Date(), e.latitude, e.longitude);
+var sunrise = times.sunrise.getHours();
+var sunset = times.sunset.getHours();
+
+
+var currentTime = new Date().getHours();
+    if (sunrise < currentTime && currentTime < sunset){
+      map.removeLayer(dark);
+      map.addLayer(light);
+    }
+    else {
+      map.removeLayer(light);
+      map.addLayer(dark);
+    }
+        }
 
 map.on('locationfound', onLocationFound); //this is the event listener
-
 function onLocationError(e) {
   alert(e.message);
 }
 
 map.on('locationerror', onLocationError);
-
 map.locate({setView: true, maxZoom: 16});
